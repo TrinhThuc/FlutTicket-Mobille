025362d8fac3408e8a58dbd474b833248d3a050c
thuctv <109392364+Judy942@users.noreply.github.com>
2025-06-04 23:34:50 +0700

    Update buy_ticket_screen.dart

--- events_app/lib/presentation/buy_ticket_screen.dart
+++ events_app/lib/presentation/buy_ticket_screen.dart
@@ -24,7 +24,7 @@ class _BuyTicketScreenState extends State<BuyTicketScreen> {
   @override
   void initState() {
     super.initState();
-    // AuthUtils.checkLogin(context);
+    AuthUtils.checkLogin(context);
   }
 
   void _updateQuantity(int index, int quantity) {
